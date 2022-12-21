d0c3659bc5f8124db0237c64e29483709b47e9a0
Vladimir Bataller <vladimirbataller@yahoo.es>
2022-12-21 15:55:28 +0100

    feat: getProducts paginated

--- api/products.js
+++ api/products.js
@@ -1,22 +1,52 @@
-import { createConnection } from "mysql2";
-export default (request, response) => {
-    var _a;
-    if (!process.env.DATABASE_URL) {
-        response.status(500).send("Server config problem");
-        return;
-    }
-    if (request.method !== 'GET') {
-        response.status(405).send("Method Not Allowed");
-        return;
-    }
-    const id = getPathParam((_a = request.url) !== null && _a !== void 0 ? _a : '');
-    const connection = createConnection(process.env.DATABASE_URL);
-    const products = runParameterizedSQLQuery(connection, 'select * from PRODUCTS where id = ?', [id]);
-    response.send(products);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-function runParameterizedSQLQuery(connection, sql, fields) {
+import { createConnection } from "mysql2";
+const handler = (request, response) => __awaiter(void 0, void 0, void 0, function* () {
+    const DATABASE_URL = process.env.DATABASE_URL;
+    if (!isRequestOk(request, response)) {
+        return;
+    }
+    const id = request.query["id"];
+    const page = request.query["page"];
+    const pageSize = request.query["pageSize"];
+    const connection = createConnection(DATABASE_URL);
+    let rawProducts = [];
+    if (id) {
+        rawProducts = yield getProductsById(connection, id);
+    }
+    else if (page && pageSize) {
+        rawProducts = yield getProducts(connection, page, pageSize);
+    }
+    else {
+        response.status(400).send("Invalid input parámeters");
+        return;
+    }
+    const products = deserializeFields(rawProducts);
+    console.log("Length", products.length);
+    response.send(products);
+});
+function getProductsById(connection, id) {
+    const sql = "select * from PRODUCTS where id = ?";
+    return promisedQuery(connection, sql, [id]);
+}
+function getProducts(connection, pageStr, pageSizeStr) {
+    const sql = "select * from PRODUCTS limit ?,?";
+    const page = parseInt(pageStr);
+    const pageSize = parseInt(pageSizeStr);
+    const jump = pageSize * (page - 1);
+    return promisedQuery(connection, sql, [jump, pageSize]);
+}
+function promisedQuery(connection, sql, params) {
     return new Promise((resolve, reject) => {
-        connection.query(sql, [...fields], (err, result, fields) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        connection.query(sql, [...params], (err, result) => {
             if (!err) {
                 resolve(result);
             }
@@ -27,11 +57,35 @@ function runParameterizedSQLQuery(connection, sql, fields) {
         });
     });
 }
-function getPathParam(incomingUrl) {
-    const urlObj = new URL(incomingUrl);
-    const { pathname } = urlObj;
-    const paths = pathname.split('/');
-    console.log(paths);
-    return paths.length >= 4 ? paths[3] : '';
+function deserializeFields(rawProducts) {
+    return rawProducts.map((product) => deserializeFieldProduct(product));
 }
-// console.log(getPathParam('https://the-phone-ka9oo8o78-vladimirbat.vercel.app/api/products/abc123'));
+function deserializeFieldProduct(product) {
+    product.options = JSON.parse(product.options);
+    product.price = JSON.parse(product.price);
+    return Object.assign({}, product);
+}
+function isRequestOk(request, response) {
+    const { DATABASE_URL } = process.env;
+    if (!DATABASE_URL) {
+        response.status(500).send("Server config problem");
+        return false;
+    }
+    if (request.method !== "GET") {
+        response.status(405).send("Method Not Allowed");
+        return false;
+    }
+    return true;
+}
+const allowCors = (fn) => (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+    if (req.method === "OPTIONS") {
+        res.status(200).end();
+        return;
+    }
+    return yield fn(req, res);
+});
+module.exports = allowCors(handler);
--- src/products.ts
+++ src/products.ts
@@ -1,56 +1,94 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import { createConnection, Connection, QueryError, FieldPacket } from "mysql2";
+import { createConnection, Connection, QueryError } from "mysql2";
+import { Product } from "./model/products.model";
 
-export default async (request: VercelRequest, response: VercelResponse) => {
-  const { DATABASE_URL } = process.env;
-  console.log("DATABASE_URL", DATABASE_URL);
-  if (!DATABASE_URL) {
-    response.status(500).send("Server config problem");
-    return;
-  }
-  if (request.method !== "GET") {
-    response.status(405).send("Method Not Allowed");
+type HandlerFunction = (request: VercelRequest, response: VercelResponse) => void;
+
+const handler = async (request: VercelRequest, response: VercelResponse) => {
+  const DATABASE_URL = process.env.DATABASE_URL as string;
+  if (!isRequestOk(request, response)) {
     return;
   }
   const id = request.query["id"] as string;
+  const page = request.query["page"] as string;
+  const pageSize = request.query["pageSize"] as string;
   const connection = createConnection(DATABASE_URL);
-  const rawProducts = await runParameterizedSQLQuery(
-    connection,
-    "select * from PRODUCTS where id = ?",
-    [id]
-  );
+  let rawProducts = [];
+  if (id) {
+    rawProducts = await getProductsById(connection, id);
+  } else if (page && pageSize ) {
+    rawProducts = await getProducts(connection, page, pageSize);
+  } else {
+    response.status(400).send("Invalid input parámeters");
+    return;
+  }
   const products = deserializeFields(rawProducts);
   console.log("Length", products.length);
   response.send(products);
 };
 
-function runParameterizedSQLQuery(
-  connection: Connection,
-  sql: string,
-  fields: string[]
-): Promise<any[]> {
+function getProductsById(connection: Connection, id: string): Promise<Product[]> {
+  const sql = "select * from PRODUCTS where id = ?";
+  return promisedQuery(connection, sql, [id]);
+}
+function getProducts(connection: Connection, pageStr: string, pageSizeStr: string): Promise<Product[]> {
+  const sql = "select * from PRODUCTS limit ?,?";
+  const page = parseInt(pageStr);
+  const pageSize = parseInt(pageSizeStr);
+  const jump = pageSize * (page - 1);
+  return promisedQuery(connection, sql, [jump, pageSize]);
+}
+
+function promisedQuery(connection: Connection, sql: string, params: unknown[]): Promise<Product[]> {
   return new Promise((resolve, reject) => {
-    connection.query(
-      sql,
-      [...fields],
-      (err: QueryError | null, result: any[], fields: FieldPacket[]) => {
-        if (!err) {
-          resolve(result);
-        } else {
-          console.log("err =>", err);
-          reject(err);
-        }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    connection.query(sql, [...params], (err: QueryError | null, result: any[]) => {
+      if (!err) {
+        resolve(result);
+      } else {
+        console.log("err =>", err);
+        reject(err);
       }
-    );
+    });
   });
 }
 
-function deserializeFields(rawProducts: any[]): any[] {
+function deserializeFields(rawProducts: Product[]): Product[] {
   return rawProducts.map((product) => deserializeFieldProduct(product));
 }
 
-function deserializeFieldProduct(product: any): any {
-  product.options = JSON.parse(product.options);
-  product.price = JSON.parse(product.price);
-  return {...product};
+function deserializeFieldProduct(product: Product): Product {
+  product.options = JSON.parse(product.options as string);
+  product.price = JSON.parse(product.price as string);
+  return { ...product };
 }
+
+function isRequestOk(request: VercelRequest, response: VercelResponse): boolean {
+  const { DATABASE_URL } = process.env;
+  if (!DATABASE_URL) {
+    response.status(500).send("Server config problem");
+    return false;
+  }
+  if (request.method !== "GET") {
+    response.status(405).send("Method Not Allowed");
+    return false;
+  }
+  return true;
+}
+
+const allowCors = (fn: HandlerFunction) => async (req: VercelRequest, res: VercelResponse) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+module.exports = allowCors(handler);
